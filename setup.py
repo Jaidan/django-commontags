9b643bbe36cfc0d605b1bca25f9fb0fd5d1015ed
John Williams <john@creoagency.com>
2012-04-18 17:49:21 -0700

    Updated setup.py for initial distribution

--- setup.py
+++ setup.py
@@ -5,6 +5,25 @@ setup(
     name="django-commontags",
     version=get_version().replace(' ', '-'),
     packages=['commontags',],
-    long_description=open('README').read(),
+    author="42nd Design and CREO Agency",
+    long_description=open('README.rst').read(),
     description="Common django template tags and filters",
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: User Interfaces",
+    ],
+    url="http://pypi.python/ord/pypi/django-commontags",
+    license="LICENSE",
+    install_requires=[
+        "Django >= 1.1.1",
+    ]
 )
